c7ef156c1f800bda30d55d586382c910e37e6701
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-17 21:16:46 -0400

    Creando el contexto y añadiendo la entidad Usuarios

---
+++ InventoryManager/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManager.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace InventoryManager.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios{ get; set; }
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
